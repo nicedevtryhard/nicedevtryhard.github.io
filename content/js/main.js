c4b1a56b3638ea84f7aaf1a897cfe2e64366a61e
nicedevtryhard <artsemkungurtsev@gmail.com>
2019-11-04 17:18:59 +0300

    adding menu button

--- content/js/main.js
+++ content/js/main.js
@@ -1,22 +1,11 @@
-let linkRequest = {
-  destination: "http://nicedevtryhard.byethost16.com/",
-  domain: { fullName: "rebrand.ly" }
-  //, slashtag: "nicedev"
-  //, title: "My Portfolio"
-}
+$(() => {
+  $('.menuToggle').on('click', () => {
+      $('.menu').slideToggle(400, () => {
 
-let requestHeaders = {
-  "Content-Type": "application/json",
-  "apikey": "54c95134525245b69d721db185022584"
-}
+          if($('.menu').css('display') === 'none'){
+              $('.menu').removeAttr('style');
+          }
 
-$.ajax({
-  url: "https://api.rebrandly.com/v1/links",
-  type: "post",
-  data: JSON.stringify(linkRequest),
-  headers: requestHeaders,
-  dataType: "json",
-  success: (link) => {
-    console.log(`Long URL was ${link.destination}, short URL is ${link.shortUrl}`);
-  }
+      });
+  });
 });
