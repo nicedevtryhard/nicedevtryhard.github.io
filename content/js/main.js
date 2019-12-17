8f33343dd337d5815ad13602c531c94c024fb1e2
nicedevtryhard <artsemkungurtsev@gmail.com>
2019-12-17 09:34:08 +0300

    Working

--- content/js/main.js
+++ content/js/main.js
@@ -1,13 +1,13 @@
 $(() => {
-  $('.menuToggle').on('click', () => {
-      $('.menu').slideToggle(400, () => {
+	$('.menuToggle').on('click', () => {
+		$('.menu').slideToggle(400, () => {
 
-          if($('.menu').css('display') === 'none'){
+			if($('.menu').css('display') === 'none'){
               $('.menu').removeAttr('style');
-          }
+			}
 
-      });
-  });
+		});
+	});
 	$(window).on('resize', () => {
 		if ($(window).width() > 559) {
 			console.log('559');
